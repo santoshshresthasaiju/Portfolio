49b93db5f48c4f6370e203a4fa502db3d257569f
Santosh Shrestha <santoshshrestha002@gmail.com>
2024-11-21 23:07:20 +0545

    navbar design

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,18 +12,20 @@ function Navbar() {
   };
 
   return (
-    <nav className='bg-gray-800 text-white px-8 md:px-16 lg:px-24'>
-      <div className='container py-4 flex justify-between items-center'>
-        <div className='flex items-center'>
-          <img src={logo} alt="Logo" className="h-10 w-35" />
+    <nav className="bg-gray-800 text-white px-8 md:px-16 lg:px-24">
+      <div className="container py-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="h-10 w-auto" />
         </div>
-        <div className='md:hidden'>
-          <button onClick={toggleMenu} className='text-white hover:text-gray-400'>
+        
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white hover:text-gray-400">
             <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="lg" />
           </button>
         </div>
-        <ul className={`md:flex space-x-4 text-white ${isOpen ? 'block' : 'hidden'} md:block`}>
-          <li><a href="#home" className="hover:text-gray-400" style={{ margin: '0 0 0 16px' }}>Home</a></li>
+
+        <ul className={`md:flex space-x-4 ${isOpen ? 'block' : 'hidden'} md:block`}>
+          <li><a href="#home" className="hover:text-gray-400">Home</a></li>
           <li><a href="#about" className="hover:text-gray-400">About</a></li>
           <li><a href="#services" className="hover:text-gray-400">Services</a></li>
           <li><a href="#projects" className="hover:text-gray-400">Projects</a></li>
@@ -42,17 +44,17 @@ function Navbar() {
         </div>
       </div>
       <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
-          <div className="flex space-x-4 py-4 justify-center">
-            <a href="https://github.com/santoshshresthasaiju" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-              <FontAwesomeIcon icon={faGithub} size="lg" />
-            </a>
-            <a href="https://www.instagram.com/santoshshersthasaiju" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-              <FontAwesomeIcon icon={faInstagram} size="lg" />
-            </a>
-            <a href="https://www.linkedin.com/in/santosh-shrestha-saiju" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-              <FontAwesomeIcon icon={faLinkedin} size="lg" />
-            </a>
-          </div>
+        <div className="flex space-x-4 py-4 justify-center">
+          <a href="https://github.com/santoshshresthasaiju" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+          </a>
+          <a href="https://www.instagram.com/santoshshersthasaiju" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
+          </a>
+          <a href="https://www.linkedin.com/in/santosh-shrestha-saiju" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+            <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          </a>
+        </div>
       </div>
     </nav>
   );
